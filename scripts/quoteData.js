74c38ec334045220ab85c9058754393692578cc2
Trevor <trevorwerner@hotmail.com>
2021-07-14 20:28:41 -0400

    Added quote data and basic template, needs work

---
+++ scripts/quoteData.js
@@ -0,0 +1,52 @@
+const quotes = [
+   {
+      quote: "Give me a museum and I'll fill it.",
+      source: "Pablo Picasso",
+      "category": "Famous"
+  },
+  {
+      quote: "Never interrupt your enemy when he is making a mistake.",
+      source: "Napoleon Bonaparte",
+      "category": "Famous"
+  },
+  {
+      quote: "If you can count your money, you don't have a billion dollars.",
+      source: "J. Paul Getty",
+      "category": "Famous"
+  },
+  {
+      quote: "This book fills a much-needed gap.",
+      source: "Moses Hadas in a review",
+      "category": "Famous"
+  },
+  {
+      quote: "Some cause happiness wherever they go; others, whenever they go.",
+      source: "Oscar Wilde",
+      "category": "Famous"
+  },
+  {
+      quote: "I have never killed anyone, but I have read some obituary notices with great satisfaction.",
+      source: "Clarence Darrow",
+      "category": "Famous"
+  },
+  {
+      quote: "I don't know why we are here, but I'm pretty sure that it is not in order to enjoy ourselves.",
+      source: "Ludwig Wittgenstein",
+      "category": "Famous"
+  },
+  {
+      quote: "Whenever I climb I am followed by a dog called 'Ego'.",
+      source: "Friedrich Nietzsche",
+      "category": "Famous"
+  },
+  {
+      quote: "He is one of those people who would be enormously improved by death.",
+      source: "H. H. Munro",
+      "category": "Famous"
+  },
+  {
+      quote: "Give me chastity and continence, but not yet.",
+      source: "Saint Augustine",
+      "category": "Famous"
+  }
+];
